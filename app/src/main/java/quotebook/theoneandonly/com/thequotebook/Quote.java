0b2e02ca23cbb5bb2b051657e1493d952ddfc229
Will Godfrey <will@williamsgodfrey.com>
2015-03-21 10:25:35 -0500

    Initial commit

---
+++ app/src/main/java/quotebook/theoneandonly/com/thequotebook/Quote.java
@@ -0,0 +1,28 @@
+package quotebook.theoneandonly.com.thequotebook;
+
+/**
+ * Created by Will and Jane on 3/17/2015.
+ */
+public class Quote {
+
+    public String quote;
+    public String person;
+
+
+    public Quote(String mQuote, String mPerson){
+        super();
+
+        quote = mQuote;
+        person = mPerson;
+
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+}
